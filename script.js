d2ef4e4bda8983c9e7d4d3e1069d1c33694e8c07
Lucas Bielefeldt <lucasbielefeldt@gmail.com>
2022-07-19 22:12:09 -0300

    edit e delete posts

--- script.js
+++ script.js
@@ -29,11 +29,19 @@ let createPost = () => {
     <div>
       <p>${data.text}</p>
       <span class="options">
-        <i class="fas fa-edit"></i>
-        <i class="fas fa-trash-alt"></i>
+        <i onClick="editPost(this)" class="fas fa-edit"></i>
+        <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
       </span>
     </div>
     `;
     input.value = "";
 };
 
+let editPost = (e) => {
+    input.value = e.parentElement.previousElementSibling.innerHTML;
+    e.parentElement.parentElement.remove();
+};
+
+let deletePost = (e) => {
+    e.parentElement.parentElement.remove();
+};
